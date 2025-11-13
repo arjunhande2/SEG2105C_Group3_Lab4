df0e5f2e990ec99f981c477d4f6a6d34fb9f18e0
Abdullah <66182508+zahinabrer5@users.noreply.github.com>
2025-11-13 09:39:40 -0500

    Fix test method name casing

--- SEG2105C-Group3-Lab4-main/app/src/test/java/com/example/mycalculator/MathEvalTest.java
+++ SEG2105C-Group3-Lab4-main/app/src/test/java/com/example/mycalculator/MathEvalTest.java
@@ -4,42 +4,42 @@ import static org.junit.Assert.*;
 
 public class MathEvalTest {
     @Test
-    public void Test1Add() {
+    public void test1Add() {
         assertEquals("12",MathEval.eval("3+9"));
     }
 
     @Test
-    public void Test2AddMult() {
+    public void test2AddMult() {
         assertEquals("9",MathEval.eval("3+2*3"));
     }
 
     @Test
-    public void Test3Div() {
+    public void test3Div() {
         assertEquals("7",MathEval.eval("16/2-1"));
     }
 
     @Test
-    public void Test4Brackets() {
+    public void test4Brackets() {
         assertEquals("10",MathEval.eval("(2+3)*2"));
     }
 
     @Test
-    public void Test5Error1() {
+    public void test5Error1() {
         assertEquals("Error",MathEval.eval("3*-"));
     }
 
     @Test
-    public void Test6Error2() {
+    public void test6Error2() {
         assertEquals("Error",MathEval.eval(""));
     }
 
     @Test
-    public void Test7Error3() {
+    public void test7Error3() {
         assertEquals("Error",MathEval.eval("(3+"));
     }
 
     @Test
-    public void DedicatedFailTest() {
+    public void dedicatedFailTest() {
         assertEquals("1",MathEval.eval("1+1"));
     }
 }
